b70542b8e4dc7d152b6866712ce3ace5ef88cf5b
EriksK82 <eriks.karklins82@gmail.com>
2026-01-28 19:51:05 +0200

    tresais

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
-print("hello world")
+# vienas rindas komentārs
+# vēl kautkāds kometārs
+
+""" Šādi var ierakstīt komentārus vairākās rindās
+    Tas ir ērti, ja komentārs ir garš
+    
+    Dažas VS code komandas saīsnes:
+    - Ctrl + /                              - komentāru pievienošana/ noņemšana rind
+    - Alt + augšup/ lejup bultiņa           - rindu pārvietošana augšup/ lejup
+    - Alt + z                               - teksta aplocīšana
+    - Shift + Alt + augšup/ lejup bultiņa   - rindu kopēšana augšup/ lejup
+"""
+
+print("hello world")  # izvada tekstu "hello world" uz ekrāna
+
+print("Šis ir otrais print")  # izvada tekstu "Šis ir otrais print" uz ekrāna fdsagdfsgadfsgafdsgfdgfdghsfdhssssssssssssssssssssssssssssssssssssssssssss 
+print("Šis ir otrais print")  # izvada tekstu "Šis ir otrais print" uz ekrāna
+print("Šis ir otrais print")  # izvada tekstu "Šis ir otrais print" uz ekrāna
+print("Šis ir otrais print")  # izvada tekstu "Šis ir otrais print" uz ekrāna
